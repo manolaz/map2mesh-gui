b72e29115ffb261700339dbc0cab18d6b4080e91
Tristan Nguyen <trungupdate@gmail.com>
2020-08-31 13:34:05 +0700

    planet-app
    Signed-off-by: Tristan Nguyen <trungupdate@gmail.com>

---
+++ build-planets-qml-Desktop_Qt_5_15_0_GCC_64bit-Debug/qmlcache_loader.cpp
@@ -0,0 +1,162 @@
+#include <QtQml/qqmlprivate.h>
+#include <QtCore/qdir.h>
+#include <QtCore/qurl.h>
+
+namespace QmlCacheGeneratedCode {
+namespace _0x5f__PlanetEffect_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__PlanetFrameGraph_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__StyledSlider_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__InfoSheet_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__ShadowEffect_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__PlanetMaterial_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__AppleTVInput_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__SolarSystem_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__Planet_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__SunEffect_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__Ring_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__PlanetsLight_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__PlanetsMain_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__FpsDisplay_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__PlanetButton_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__planets_js { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+
+}
+namespace {
+struct Registry {
+    Registry();
+    ~Registry();
+    QHash<QString, const QQmlPrivate::CachedQmlUnit*> resourcePathToCachedUnit;
+    static const QQmlPrivate::CachedQmlUnit *lookupCachedUnit(const QUrl &url);
+};
+
+Q_GLOBAL_STATIC(Registry, unitRegistry)
+
+
+Registry::Registry() {
+        resourcePathToCachedUnit.insert(QStringLiteral("/PlanetEffect.qml"), &QmlCacheGeneratedCode::_0x5f__PlanetEffect_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/PlanetFrameGraph.qml"), &QmlCacheGeneratedCode::_0x5f__PlanetFrameGraph_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/StyledSlider.qml"), &QmlCacheGeneratedCode::_0x5f__StyledSlider_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/InfoSheet.qml"), &QmlCacheGeneratedCode::_0x5f__InfoSheet_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/ShadowEffect.qml"), &QmlCacheGeneratedCode::_0x5f__ShadowEffect_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/PlanetMaterial.qml"), &QmlCacheGeneratedCode::_0x5f__PlanetMaterial_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/AppleTVInput.qml"), &QmlCacheGeneratedCode::_0x5f__AppleTVInput_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/SolarSystem.qml"), &QmlCacheGeneratedCode::_0x5f__SolarSystem_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/Planet.qml"), &QmlCacheGeneratedCode::_0x5f__Planet_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/SunEffect.qml"), &QmlCacheGeneratedCode::_0x5f__SunEffect_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/Ring.qml"), &QmlCacheGeneratedCode::_0x5f__Ring_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/PlanetsLight.qml"), &QmlCacheGeneratedCode::_0x5f__PlanetsLight_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/PlanetsMain.qml"), &QmlCacheGeneratedCode::_0x5f__PlanetsMain_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/FpsDisplay.qml"), &QmlCacheGeneratedCode::_0x5f__FpsDisplay_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/PlanetButton.qml"), &QmlCacheGeneratedCode::_0x5f__PlanetButton_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/planets.js"), &QmlCacheGeneratedCode::_0x5f__planets_js::unit);
+    QQmlPrivate::RegisterQmlUnitCacheHook registration;
+    registration.version = 0;
+    registration.lookupCachedQmlUnit = &lookupCachedUnit;
+    QQmlPrivate::qmlregister(QQmlPrivate::QmlUnitCacheHookRegistration, &registration);
+}
+
+Registry::~Registry() {
+    QQmlPrivate::qmlunregister(QQmlPrivate::QmlUnitCacheHookRegistration, quintptr(&lookupCachedUnit));
+}
+
+const QQmlPrivate::CachedQmlUnit *Registry::lookupCachedUnit(const QUrl &url) {
+    if (url.scheme() != QLatin1String("qrc"))
+        return nullptr;
+    QString resourcePath = QDir::cleanPath(url.path());
+    if (resourcePath.isEmpty())
+        return nullptr;
+    if (!resourcePath.startsWith(QLatin1Char('/')))
+        resourcePath.prepend(QLatin1Char('/'));
+    return unitRegistry()->resourcePathToCachedUnit.value(resourcePath, nullptr);
+}
+}
+int QT_MANGLE_NAMESPACE(qInitResources_planets_qml)() {
+    ::unitRegistry();
+    Q_INIT_RESOURCE(planets_qml_qmlcache);
+    return 1;
+}
+Q_CONSTRUCTOR_FUNCTION(QT_MANGLE_NAMESPACE(qInitResources_planets_qml))
+int QT_MANGLE_NAMESPACE(qCleanupResources_planets_qml)() {
+    Q_CLEANUP_RESOURCE(planets_qml_qmlcache);
+    return 1;
+}
